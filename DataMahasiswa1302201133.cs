6b736bbef98794218fc869bb65d2ce2149f8d90c
fiqril <mfikrilh@gmail.com>
2022-03-29 23:02:19 -0700

    membuat class DataMahasiswa1302201133

---
+++ DataMahasiswa1302201133.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace modul6_kelompok_CodeBreaker
+{
+    public class DataMahasiswa1302201133
+    {
+        
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+
+        public class Address
+        {
+            public string streetAddress { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+        }
+
+        public class Cours
+        {
+            public string code { get; set; }
+            public string name { get; set; }
+        }
+
+
+
+        public static DataMahasiswa1302201133 fromJson(FileStream json)
+        {
+            var student = JsonSerializer.Deserialize<DataMahasiswa1302201133>(json);
+            return student;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace modul6_kelompok_CodeBreaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\pc\source\repos\tpmodul7_1302201133\modul6_kelompok_CodeBreaker\bin\Debug\net6.0\jurnal6_1_1302201133.json";
+            using FileStream json = File.OpenRead(path);
+            var student = DataMahasiswa1302201133.fromJson(json);
+
+            Console.WriteLine(student.firstName);
+            Console.WriteLine(student.lastName);
+            Console.WriteLine(student.gender);
+            Console.WriteLine(student.age);
+            
+        }
+    }
+}
