a5d52e7a6d39cfb00b43145f0ace7903eeffb0e0
habibmirsyad <habmirs@gmail.com>
2022-04-02 14:46:17 +0700

    finishing class DataMahasiswa1302201138

--- DataMahasiswa1302201138.cs
+++ DataMahasiswa1302201138.cs
@@ -37,9 +37,23 @@ namespace modul6_1302201138
             string file = "jurnal6_1_1302201138.json";
             string strFile = File.ReadAllText(file);
             Personal personal = JsonSerializer.Deserialize<Personal>(strFile);
-            
 
-            Console.WriteLine($"Mahasiswa {personal.firstName} {personal.lastName} berumur {personal.age} tahun, memiliki jenis kelamin {personal.gender}");
+            Console.WriteLine($"==Personal Data==\n" +
+                $"Name\t: {personal.firstName} {personal.lastName}\n" +
+                $"Nick\t: {personal.firstName}\n" +
+                $"Age\t: {personal.age}\n" +
+                $"Gender\t: {personal.gender}");
+
+            Console.WriteLine($"\n==Address==\n" +
+                $"Street\t: {personal.address.streetAddress}\n" +
+                $"City\t: {personal.address.city}\n" +
+                $"State\t: {personal.address.state}");
+
+            Console.WriteLine($"\n==Courses taken==");
+            for (int i = 0; i < personal.courses.Length; i++)
+            {
+                Console.WriteLine($"{personal.courses[i].code} - {personal.courses[i].name}");
+            }
 
         }
     }
