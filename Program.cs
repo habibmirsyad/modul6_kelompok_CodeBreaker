980c9c26cac7dfc87fd58c35ccf4876701ced67d
AnugrahSA <100453661+AnugrahSA@users.noreply.github.com>
2022-04-02 15:12:45 +0800

    Revert "Anugrah"

--- Program.cs
+++ Program.cs
@@ -1,19 +1,2 @@
-﻿using System;
-
-namespace modul6_kelompok_CodeBreaker
-{
-    public class main
-    {
-        public static void Main()
-        {
-            DataMahasiswa1302204047 mhs1 = new DataMahasiswa1302204047();
-            mhs1.ReadJSON();
-
-            TeamMembers1302204047 member1 = new TeamMembers1302204047();
-            member1.ReadJSON();
-
-            GlossaryItem1302204047 glossary = new GlossaryItem1302204047();
-            glossary.ReadJSON();
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
