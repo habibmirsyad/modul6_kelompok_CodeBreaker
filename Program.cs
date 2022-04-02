c178c1f62e677b94c6d8795849153811d26236f9
Aditya MP <211wadityaw112@gmail.com>
2022-04-02 14:09:39 +0700

    menambahkan class GlossaryItem1302204054

--- Program.cs
+++ Program.cs
@@ -6,8 +6,13 @@ class main
     {
         DataMahasiswa1302204054 mhsA1 = new DataMahasiswa1302204054();
         mhsA1.ReadJSON();
+        Console.WriteLine();
 
         TeamMembers1302204054 mhsA2 = new TeamMembers1302204054();
         mhsA2.ReadJSON();
+        Console.WriteLine();
+
+        GlossaryItem1302204054 mhsA3 = new GlossaryItem1302204054();
+        mhsA3.ReadJSON();
     }
 }
